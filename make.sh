b80ae456ce197cd711ebb650c9253e035d6b9c32
Giulianos <scaglionigiuliano@gmail.com>
2018-10-28 03:53:44 -0300

    small changes in config files

--- make.sh
+++ make.sh
@@ -1,10 +1,10 @@
-echo "Creating tars"
-mvn clean install eclipse:eclipse
+# compile
+mvn clean install
 
-echo "Generating node scripts"
+# extract server files
 tar -xf server/target/tpe-server-1.0-SNAPSHOT-bin.tar.gz -C server/target
 chmod +x server/target/tpe-server-1.0-SNAPSHOT/run-node.sh
 
-echo "Generating client scripts"
+# extract client files
 tar -xf client/target/tpe-client-1.0-SNAPSHOT-bin.tar.gz -C client/target
 chmod +x client/target/tpe-client-1.0-SNAPSHOT/run-client.sh
